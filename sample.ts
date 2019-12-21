aeb06787cd3931822eddc808045c49fc1cca28d7
narumincho <narumincho.starfy@gmail.com>
2019-12-21 19:26:10 +0900

    read export variable

--- sample.ts
+++ sample.ts
@@ -1,1 +1,20 @@
-export const data = "sorena";
+export const constantValue = "stringValue";
+
+/** ユーザー */
+type User = {
+  /** 名前 */
+  name: string;
+  /** 年齢 */
+  age: number;
+  /** 作成日時 */
+  createdAt: Date;
+};
+
+/** IDからユーザーの情報を取得する */
+export const getUser = async (id: string): Promise<User> => {
+  return {
+    name: "Kish.",
+    age: 21,
+    createdAt: new Date()
+  };
+};
--- source/index.ts
+++ source/index.ts
@@ -1,12 +1,50 @@
 import * as ts from "typescript";
 
-const getExposedVariable = (fileName: string, option: ts.CompilerOptions) => {
+// 参考: https://github.com/microsoft/TypeScript/wiki/Using-the-Compiler-API
+
+const visit = (node: ts.Node): void => {
+  if (ts.isStringLiteral(node)) {
+    console.log(node.text);
+  }
+  node.forEachChild(visit);
+};
+
+const getExposedVariable = (
+  fileName: string,
+  option: ts.CompilerOptions
+): void => {
   const program = ts.createProgram({
     rootNames: [fileName],
     options: option
   });
   const typeChecker = program.getTypeChecker();
-  console.log(program);
+
+  const sourceFile = program.getSourceFile(fileName);
+  if (sourceFile === undefined) {
+    throw new Error("指定したファイルをソースファイルとして認識できなかった");
+  }
+  if (sourceFile.isDeclarationFile) {
+    throw new Error("指定したファイルが型定義ファイル(.d.ts)だった");
+  }
+  if (!ts.isExternalModule(sourceFile)) {
+    throw new Error(
+      "外部へのエクスポートされる関数があるモジュールではなかった"
+    );
+  }
+  const symbolTable = ((sourceFile as unknown) as { symbol: ts.Symbol }).symbol
+    .exports;
+  if (symbolTable === undefined) {
+    console.log("symbolTableを取得できなかった");
+    return;
+  }
+  symbolTable.forEach((value, key) => {
+    console.log(key);
+    for (const declaration of value.declarations) {
+      declaration.forEachChild(visit);
+    }
+  });
 };
 
-getExposedVariable("sample.ts", {});
+getExposedVariable("sample.ts", {
+  target: ts.ScriptTarget.ES2019
+});
