0a1b98c1d083f6da2eb0d373c130458a6b47f08e
narumincho <narumincho.starfy@gmail.com>
2020-01-07 22:25:47 +0900

    use ts-morph

--- source/emitter.ts
+++ source/emitter.ts
@@ -1,49 +1,26 @@
 import * as ts from "typescript";
 import * as type from "./type";
+import * as tsm from "ts-morph";
 
-export const emit = (serverCode: type.ServerCode): string => {
-  return ts
-    .createVariableDeclarationList(
-      [
-        ts.createVariableDeclaration(
-          ts.createIdentifier("functionWithOutReturnType"),
-          undefined,
-          ts.createArrowFunction(
-            undefined,
-            undefined,
-            [
-              ts.createParameter(
-                undefined,
-                undefined,
-                undefined,
-                ts.createIdentifier("id"),
-                undefined,
-                ts.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
-                undefined
-              )
-            ],
-            undefined,
-            ts.createToken(ts.SyntaxKind.EqualsGreaterThanToken),
-            ts.createBlock(
-              [
-                ts.createReturn(
-                  ts.createObjectLiteral(
-                    [
-                      ts.createPropertyAssignment(
-                        ts.createIdentifier("data"),
-                        ts.createNumericLiteral("32")
-                      )
-                    ],
-                    true
-                  )
-                )
-              ],
-              true
-            )
-          )
-        )
-      ],
-      ts.NodeFlags.Const
-    )
-    .getText();
+export const emit = (serverCode: type.ServerCode): void => {
+  const project = new tsm.Project({
+    compilerOptions: {
+      strict: true
+    }
+  });
+
+  project.createSourceFile("out.ts", {
+    statements: [
+      {
+        kind: tsm.StructureKind.Enum,
+        name: "MyEnum",
+        members: [
+          {
+            name: "member"
+          }
+        ]
+      }
+    ]
+  });
+  project.saveSync();
 };
