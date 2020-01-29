65342695f032456aaa97e837d7305a2e030f7bc3
narumincho <narumincho.starfy@gmail.com>
2020-01-29 12:12:39 +0900

    use jstscodegenerator@0.0.4

--- source/emitter.ts
+++ source/emitter.ts
@@ -1,6 +1,7 @@
 import * as type from "./type";
 import * as generator from "jstscodegenerator";
 import * as fs from "fs";
+import { expr, typeExpr } from "jstscodegenerator";
 
 export const emit = (
   serverCode: type.ServerCode,
@@ -13,10 +14,10 @@ export const emit = (
     >("express", ["Request", "Response"], []);
     const nodeJsCode: generator.NodeJsCode = {
       exportTypeAliasList: [],
-      exportVariableList: [
+      exportFunctionList: [
         {
           name: "middleware",
-          typeExpr: generator.typeExpr.functionReturnVoid([
+          parameterList: [
             {
               name: "request",
               document: "リクエスト",
@@ -27,9 +28,49 @@ export const emit = (
               document: "レスポンス",
               typeExpr: expressModule.typeList.Response
             }
-          ]),
-          document: "ミドルウェア",
-          expr: generator.stringLiteral("まだ途中")
+          ],
+          returnType: null,
+          statementList: [
+            expr.variableDefinition(
+              typeExpr.union([typeExpr.typeString, typeExpr.typeUndefined]),
+              expr.getProperty(
+                expr.getProperty(expr.argument(0, 0), "headers"),
+                "accept"
+              )
+            ),
+            expr.ifStatement(
+              expr.logicalAnd(
+                expr.notEqual(expr.localVariable(0, 0), expr.undefinedLiteral),
+                expr.call(
+                  expr.getProperty(expr.localVariable(0, 0), "includes"),
+                  [expr.stringLiteral("text/html")]
+                )
+              ),
+              [
+                expr.evaluateExpr(
+                  expr.call(
+                    expr.getProperty(expr.argument(1, 1), "setHeader"),
+                    [
+                      expr.stringLiteral("content-type"),
+                      expr.stringLiteral("text/html")
+                    ]
+                  )
+                ),
+                expr.evaluateExpr(
+                  expr.call(expr.getProperty(expr.argument(1, 1), "send"), [
+                    expr.stringLiteral("htmlをリクエストした")
+                  ])
+                ),
+                expr.returnVoidStatement()
+              ]
+            ),
+            expr.evaluateExpr(
+              expr.call(expr.getProperty(expr.argument(0, 1), "send"), [
+                expr.stringLiteral("APIのレスポンス")
+              ])
+            )
+          ],
+          document: "ミドルウェア"
         }
       ]
     };
