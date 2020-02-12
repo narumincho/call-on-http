eefee34eda80752572dc612de33da32600189bd7
narumincho <narumincho.starfy@gmail.com>
2020-02-13 08:04:47 +0900

    add create id code

--- source/index.ts
+++ source/index.ts
@@ -4,6 +4,19 @@ import * as emitter from "./emitter";
 
 export { type };
 
+/*
+ *  IDの作成には
+ *
+ *  コマンドライン
+ *  node --eval "console.log(require('crypto').randomBytes(16).toString('hex'))"
+ *
+ *  ブラウザのコンソール (クリップボードへコピーも行う)
+ *  copy([...crypto.getRandomValues(new Uint8Array(16))].map(e=>e.toString(16).padStart(2, "0")).join(""))
+ *
+ *  を使うと便利
+ *
+ */
+
 export const generateServerCodeAndUpdateTemplate = (
   functionList: ReadonlyArray<type.ApiFunction>,
   serverCodePath: string,
