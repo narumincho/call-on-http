e4ef0bb7e4ff049953b1a6291b500912a85e4b68
narumincho <narumincho.starfy@gmail.com>
2020-02-14 20:10:11 +0900

    add comment run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,7 @@
+# sample/schema.ts から サーバーのコードを生成する
 npx.ps1 tsc --project ./sample/tsconfig.json;
 node.exe ./sampleDistribution/sample/schema.js
 
+# 生成されたサーバーのコードをそのまま実行する
 npx.ps1 tsc sample/use.ts --strict --outDir ./sampleOut
 node.exe ./sampleOut/use.js
